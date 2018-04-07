03874b8337463d02bfb38e123d4d3722201a6d4c
Kazi Rahamatullah <kazirahmat58@gmail.com>
2018-04-07 21:35:08 +0600

    Laravel + Vue Js PhoneBook App

---
+++ resources/assets/js/components/ExampleComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        I'm an example component. In vue.......
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted: function() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
+
+<!-- <template>
+    <div>
+      <h1>rahmat</h1>
+    </div>
+</template> -->
