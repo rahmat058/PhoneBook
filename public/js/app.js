30bb256f9176fa4b288294c8ecc931f24c0ae262
Kazi Rahamatullah <kazirahmat58@gmail.com>
2018-04-09 16:07:42 +0600

    Server side validation is done for input field

--- public/js/app.js
+++ public/js/app.js
@@ -46454,6 +46454,11 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 //
 //
 //
+//
+//
+//
+//
+//
 
 
 /* harmony default export */ __webpack_exports__["default"] = ({
@@ -46464,7 +46469,8 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
         name: '',
         phone: '',
         email: ''
-      }
+      },
+      errors: {}
     };
   },
   methods: {
@@ -46477,7 +46483,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
       axios.post('/phonebook', this.$data.list).then(function (response) {
         return _this.closeModal();
       }).catch(function (error) {
-        return console.log(error);
+        return _this.errors = error.response.data.errors;
       });
     }
   }
@@ -46520,6 +46526,7 @@ var render = function() {
                 }
               ],
               staticClass: "input",
+              class: { "is-danger": _vm.errors.name },
               attrs: { type: "text", placeholder: "Name" },
               domProps: { value: _vm.list.name },
               on: {
@@ -46531,7 +46538,13 @@ var render = function() {
                 }
               }
             })
-          ])
+          ]),
+          _vm._v(" "),
+          _vm.errors.name
+            ? _c("small", { staticClass: "has-text-danger" }, [
+                _vm._v(_vm._s(_vm.errors.name["0"]))
+              ])
+            : _vm._e()
         ]),
         _vm._v(" "),
         _c("div", { staticClass: "field" }, [
@@ -46548,6 +46561,7 @@ var render = function() {
                 }
               ],
               staticClass: "input",
+              class: { "is-danger": _vm.errors.phone },
               attrs: { type: "number", placeholder: "Phone" },
               domProps: { value: _vm.list.phone },
               on: {
@@ -46559,7 +46573,13 @@ var render = function() {
                 }
               }
             })
-          ])
+          ]),
+          _vm._v(" "),
+          _vm.errors.phone
+            ? _c("small", { staticClass: "has-text-danger" }, [
+                _vm._v(_vm._s(_vm.errors.phone["0"]))
+              ])
+            : _vm._e()
         ]),
         _vm._v(" "),
         _c("div", { staticClass: "field" }, [
@@ -46576,6 +46596,7 @@ var render = function() {
                 }
               ],
               staticClass: "input",
+              class: { "is-danger": _vm.errors.email },
               attrs: { type: "email", placeholder: "Email" },
               domProps: { value: _vm.list.email },
               on: {
@@ -46587,7 +46608,13 @@ var render = function() {
                 }
               }
             })
-          ])
+          ]),
+          _vm._v(" "),
+          _vm.errors.email
+            ? _c("small", { staticClass: "has-text-danger" }, [
+                _vm._v(_vm._s(_vm.errors.email["0"]))
+              ])
+            : _vm._e()
         ])
       ]),
       _vm._v(" "),
--- resources/assets/js/components/Add.vue
+++ resources/assets/js/components/Add.vue
@@ -13,20 +13,25 @@
           <div class="field">
              <label class="label">Name</label>
              <div class="control">
-               <input class="input" type="text" placeholder="Name" v-model="list.name">
+               <input class="input" :class="{'is-danger': errors.name}" type="text" placeholder="Name" v-model="list.name">
              </div>
+             <small v-if="errors.name" class="has-text-danger">{{errors.name['0']}}</small>
           </div>
+
           <div class="field">
              <label class="label">Phone</label>
              <div class="control">
-               <input class="input" type="number" placeholder="Phone" v-model="list.phone">
+               <input class="input" :class="{'is-danger': errors.phone}" type="number" placeholder="Phone" v-model="list.phone">
              </div>
+             <small v-if="errors.phone" class="has-text-danger">{{errors.phone['0']}}</small>
           </div>
+
           <div class="field">
              <label class="label">Email</label>
              <div class="control">
-               <input class="input" type="email" placeholder="Email" v-model="list.email">
+               <input class="input" :class="{'is-danger': errors.email}" type="email" placeholder="Email" v-model="list.email">
              </div>
+             <small v-if="errors.email" class="has-text-danger">{{errors.email['0']}}</small>
           </div>
 
        </section>
@@ -50,6 +55,9 @@ export default {
          name: '',
          phone: '',
          email: ''
+       },
+       errors: {
+
        }
      }
   },
@@ -59,7 +67,7 @@ export default {
     },
     saveData: function() {
         axios.post('/phonebook', this.$data.list).then((response)=> this.closeModal())
-        .catch((error)=> console.log(error));
+        .catch((error)=> this.errors = error.response.data.errors);
     }
   }
 }
